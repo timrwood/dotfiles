8141d7dc60fbb48c2f62bb8e3bafcacf750ae83e
Tim Wood <washwithcare@gmail.com>
2013-08-27 10:59:46 -0700

    use symlink instead of copying file

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git pull
 cp .bash_profile ~/.bash_profile
-cp Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User
+ln -sf ~/dotfiles/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User
 source ~/.bash_profile
